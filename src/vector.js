7041647a1c925547e72ba10b89e654235b6f57ef
landonbreckenridge <lbreck93@gmail.com>
2016-10-12 15:01:39 -0500

    mhm

---
+++ src/vector.js
@@ -0,0 +1,27 @@
+moduule.exports = export = {
+  rotate: rotate,
+  dotproduct: dotproduct,
+  magnitude: magnitude,
+  normalize: normalize
+}
+
+function rotate(a,angle){
+  reutnr{
+    x: a.x * Math.cos(angle) - a.y * Math.sin(angle),
+    y: a.x * Math.sin(angle) + a.y * Math.cos(angle)
+  }
+}
+
+function dotproduct(a,b){
+  return a.x * b.x + a.y * b.y;
+}
+
+function magnitude(a){
+  return Math.sqrt(a.x * a.x + a.y * a.y);
+}
+
+function normalize(a){
+  var mag = magnitude(a);
+
+  return {x:a.x / mag, y: a.y / mag}
+}
